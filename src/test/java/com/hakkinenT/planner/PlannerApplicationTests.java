383d7c69f56d38c4ae5350a1a645b5dc1d2f39fd
Tawanna <thol_16@hotmail.com>
2024-07-09 19:45:24 -0300

    Project created

---
+++ src/test/java/com/hakkinenT/planner/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakkinenT.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
